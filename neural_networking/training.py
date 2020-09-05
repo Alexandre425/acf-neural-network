7fa3bfecb948a4cfea33aea2b80493a2029f6b9d
Alexandre425 <alexandre.d.rodrigues@tecnico.ulisboa.pt>
2020-09-05 16:16:48 +0100

    input error

--- neural_networking/training.py
+++ neural_networking/training.py
@@ -4,38 +4,38 @@ from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
-# Neural network model
-model = Sequential([
-    Dense(8, input_shape=(8,)),
-    Dense(8, activation='relu'),
-    Dense(8, activation='relu'),
-    Dense(8)
-])
-
-X = [np.array(0)]*12
-print(X)
-print(X[0])
-
+# Make an empty list
+data = [[] for x in range(12)]
 # Opening and parsing the file
 f = open("training_data.txt", "r")
-l = 0
 for lin in f:
     vals = lin.replace("[", '').replace("]", '').split()    # Remove the brackets and split every space
     vals = [float(x) for x in vals]
     i = 0
+    # Put each of the values in it's list
     for v in vals:
-        X[i] = X[i].append(v)
+        data[i].append(v)
         i += 1
-    l += 1
-
-print(X)
-
+# Atributing the correct data to the inputs and outputs
+X = data[0:8]
+Y = data[8:12]
+# Convertyng to numpy arrays
+X = [np.array(x) for x in X]
+Y = [np.array(x) for x in Y]
 
 f.close()
+
+# Neural network model
+model = Sequential([
+    Dense(8, activation='relu', input_shape=(8,)),
+    Dense(8, activation='relu'),
+    Dense(4)
+])
 
 model.compile(
     optimizer='adam',
     loss='huber_loss',
     metrics=['mean_squared_error']
 )
-#model.fit()
+
+model.fit(x=X, y=Y, batch_size=100, epochs=5)
